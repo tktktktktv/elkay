57137f710f6940d8fe91bfce48b55066ba1b7265
tktktktktv <110785684+tktktktktv@users.noreply.github.com>
2025-05-02 14:31:41 -0400

    Update aboutLayouts.js
    updated bulleted list in "hi!" section

--- aboutLayouts.js
+++ aboutLayouts.js
@@ -3,15 +3,16 @@ const aboutLayouts = {
         <div class="about-layout">
             <p>I am a researcher and curator with more than a decade of experience working at the nexus of art, 
             technology, and communication.</p>
+            <p>I have held positions at institutions including The Museum of Modern Art, Princeton University, and <em>Artforum</em>. 
             <p>My practice is grounded in a desire to explore experimental forms and 
             networks of collective creation, particularly those emerging outside traditional frameworks.</p>
 
             <p>I am excited to explore collaborations and conversations, with a particular interest in:</p>
             <ul>
                 <li>Contemporary art</li>
-                <li>Digital culture</li>
                 <li>Blockchain / web3</li>
-                <li>Critical approaches to emerging technology</li>
+                <li>Creative approaches to human–AI cooperation</li>
+                <li>Pirate media networks</li>
             </ul>
             <p>Please <a href="mailto:lkavaliunas@icloud.com" target="_blank">reach out</a> if you'd like to work together :)</p>
         </div>
