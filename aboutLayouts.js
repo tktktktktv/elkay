d78d93803bb455f686e40c4c081a99d7a65e4f65
tktktktktv <110785684+tktktktktv@users.noreply.github.com>
2025-05-04 18:33:29 -0400

    Update aboutLayouts.js
    updated email to gmail (not icloud)

--- aboutLayouts.js
+++ aboutLayouts.js
@@ -5,7 +5,7 @@ const aboutLayouts = {
             technology, and communication.</p>
             <p>I have held positions at institutions including The Museum of Modern Art, Princeton University, and <em>Artforum</em>. 
             <p>My practice is grounded in a desire to explore experimental forms and 
-            networks of collective creation, particularly those emerging outside traditional frameworks.</p>
+            networks of collective creation, especially those emerging outside traditional frameworks.</p>
 
             <p>I am excited to explore collaborations and conversations, with a particular interest in:</p>
             <ul>
@@ -14,19 +14,19 @@ const aboutLayouts = {
                 <li>Creative approaches to human–AI cooperation</li>
                 <li>Pirate media networks</li>
             </ul>
-            <p>Please <a href="mailto:lkavaliunas@icloud.com" target="_blank">reach out</a> if you'd like to work together :)</p>
+            <p>Please <a href="mailto:kavaliunaslina@gmail.com" target="_blank">reach out</a> if you'd like to work together :)</p>
         </div>
     `,
     'CV': `
         <div class="about-layout">
         
-         <p>Full professional CV available on request – <a href="mailto:lkavaliunas@icloud.com" target="_blank">email me</a></p>
+         <p>Full professional CV available on request – <a href="mailto:kavaliunaslina@gmail.com" target="_blank">email me</a></p>
     
         </div>
     `,
     'Contact': `
         <div class="about-layout">    
-            <p>Don't hesitate to <a href="mailto:lkavaliunas@icloud.com" target="_blank">contact me</a> about something concrete or developing or really anything at all!</p>
+            <p>Don't hesitate to <a href="mailto:kavaliunaslina@gmail.com" target="_blank">contact me</a> about something concrete or developing or really anything at all!</p>
         </div>
     `
 };
